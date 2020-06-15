f858958f347daa76ee3c2654f386f31f26b6e289
johnpcooper <johnphilipcooper@gmail.com>
2020-06-14 21:08:46 -0500

    added env var setting to play_clipboard

--- spotify_jpc/playback.py
+++ spotify_jpc/playback.py
@@ -1,14 +1,16 @@
-from spotify_jpc.utilities import get_user_sp, get_clipboard_uri
+from spotify_jpc.utilities import get_user_sp, get_clipboard_uri, set_env_vars
 
 def play_clipboard(**kwargs):
     """
     Search spotify for whatever's on the clipboard and
     play the first resulting track
     """
+    set_env_vars()
     track_uri = get_clipboard_uri()
     sp = get_user_sp()
 
     if track_uri:
         sp.start_playback(uris=[track_uri])
+        print(f"Playing track")
     else:
         print("Couldn't find a track with this clipboard query")
