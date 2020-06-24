a871747e2b3653f962e2b3bb1e84c965903b61f8
johnpcooper <johnphilipcooper@gmail.com>
2020-06-24 18:18:44 -0500

    updated constants_example.py

--- spotify_jpc/constants_example.py
+++ spotify_jpc/constants_example.py
@@ -1,6 +1,7 @@
-env_vars = {'SPOTIPY_CLIENT_ID': None,
-            'SPOTIPY_CLIENT_SECRET': None,
-            'SPOTIPY_REDIRECT_URI': None, 
+env_vars = {'SPOTIPY_CLIENT_ID': '770adfb757ddac74f730a65fa6b56b496',
+            'SPOTIPY_CLIENT_SECRET': 'f1964dd3424bb37c395e7849e645c',
+            'SPOTIPY_REDIRECT_URI': 'http://localhost:9090',
             'DISPLAY': ':0'}
 
-user_vars = {'username': None}
+user_vars = {'username': 'anothergriningsoul',
+             'playlist_db_path': r"C:\spotify_jpc\notebooks\playlist_db.csv"}
