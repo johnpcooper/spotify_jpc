5dd6e42f4e6220d948a3209d814587bd711e766c
johnpcooper <johnphilipcooper@gmail.com>
2020-06-30 16:37:22 -0500

    fixed type

--- spotify_jpc/constants_example.py
+++ spotify_jpc/constants_example.py
@@ -1,20 +1,3 @@
-env_vars = {'SPOTIPY_CLIENT_ID': 'your-spotify-client-id',
-            'SPOTIPY_CLIENT_SECRET': 'your-spotify-client-secret',
-            'SPOTIPY_REDIRECT_URI': 'http://localhost:9090',
-            'DISPLAY': ':0'}
-
-scope_list = ['user-modify-playback-state',
-              'user-read-recently-played',
-              'user-read-currently-playing',
-              'playlist-modify-private',
-              'playlist-modify-public']
-              
-scope = " ".join(scope_list)
-
-user_vars = {'username': 'your-username',
-             'playlist_db_path': r"C:\spotify_jpc\notebooks\playlist_db.csv",
-			 'cache_path': r"C:\.spotify\.usercache"}
-
 def package_path(**kwargs):
     """
     Return the path to the local installation
